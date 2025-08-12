44d55953fda9f5e91753baa24362e5e8aa2519a1
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2025-08-12 06:16:55 +0000

    Lecture18 AUthentication and authorisation

---
+++ Lecture18(Authentication - Authorisation)(12Aug)/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const app = express(); 
+const mongoose = require('mongoose');
+const PORT = 3000;
+const db = require('./db/connectDb.js');
+const model = require('./Model/model.js'); // through this..we will import the model
+const todoRoute = require('./routes/todo.route.js'); // Importing the todo route......
+app.use(express.static(path.join(__dirname, "public"))); 
+
+const todoRouter = require('./routes/todo.route.js'); // Importing the todo route
+
+app.use(express.json()); //this is the middlware to parse the json data which was received ..
+
+//routers 
+app.use("/todo", todoRouter); // this is the route for the todo api
+// this will handle all the requests to the /todo route
+
+
+app.get('/', (req, res) => {
+  res.redirect('index.html'); // Redirecting to index.html in the public folder
+});
+app.use('/api/todos', todoRoute);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+/*
+API INTEGRATION -- -CONNECTING FRONTEND AND BACKEND 
+
+*/
