55fe44970b2295f4bc9d77ca1ae9fab9cea34b7c
sparshsharma81 <81arvindarora@gmail.com>
2025-10-03 10:47:55 +0530

    Postgre SQL

---
+++ Lecture30(Postgres SQL)(3oct)/index.js
@@ -0,0 +1,6 @@
+const app = express();
+const PORT = 5000;
+
+//we can not install prisma directly -- like not with npm i prisma
+//we need to completely do the setup of prisma 
+//the command is npx prisma init 
